d2f69502ebc6bf96716bab8477a2072af5ab8723
mjfre <mjfre@aol.com>
2021-03-05 21:15:09 -0800

    completed

---
+++ src/main/java/com/mjfre/springstompexample/SpringStompExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mjfre.springstompexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStompExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStompExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjfre/springstompexample/SpringStompExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjfre.springstompexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStompExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
